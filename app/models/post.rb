5d48d55e464d3555c69282bd67e9436259f8fdad
theluispestana <theluispestana@gmail.com>
2020-04-28 11:33:20 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,32 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate :clickbait?
+
+  def clickbait?
+    if !self.check_title 
+      errors.add(:title, "Needs more clickbait")
+    end
+  end
+
+  def check_title
+    if !self.title
+      return false
+    end
+
+    split_title = self.title.split(' ')
+    if self.title.include?("Won't Believe")
+      return true
+    elsif self.title.include?("Secret") 
+      return true
+    elsif self.title.include?("top") && split_title[1].is_integer?
+      return true
+    elsif self.title.include?("Guess")
+      return true
+    else
+      return false
+    end
+  end
 end
